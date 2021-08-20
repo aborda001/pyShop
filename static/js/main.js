2645aae5c9c883d6dbe4eb41dccc2665a6fbd4c5
Arnaldo Borda <aborda001@gmail.com>
2021-08-20 11:43:47 -0400

    Haciendo la peticion con ajax para el buscador

--- static/js/main.js
+++ static/js/main.js
@@ -1,7 +1,31 @@
-document.addEventListener('DOMContentLoaded', function () {
+//document.addEventListener('DOMContentLoaded', function () {
 	const botonBuscar = document.getElementById('botonBuscar');
 
 	botonBuscar.onclick = function () {
 		console.log("Presionado")	
 	}
-})
+
+	function cargarDatos(consulta) {
+		$.ajax({
+			url:"/buscador",
+			method:"POST",
+			data:{consulta:consulta},
+			success:function (data) {
+				$('#resultado').html(data)
+				$("#resultado").append(data.htmlresponse);
+				console.log(data.htmlresponse);
+			}
+		});
+	}
+
+	$('#buscador').keyup(function(){
+		let buscar = $(this).val();
+
+		if(buscar != ''){
+			cargarDatos(buscar);
+		}
+
+	});
+
+
+//})
