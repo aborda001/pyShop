8d9eb0d31be00a721c8585be5699e7d9e3423635
Arnaldo Borda <aborda001@gmail.com>
2021-10-14 21:52:49 -0300

    Implemetando la funcion del filtro para los ingresos y egresos, y la funcionalidad de agregar nuevos

--- static/js/caja.js
+++ static/js/caja.js
@@ -1,16 +1,91 @@
-document.addEventListener('DOMContentLoaded', function () {
-	function datosIngresoEgreso() {
-		consulta = "listaIngresoEgreso"
+document.addEventListener('DOMContentLoaded', function() {
+	const btnHoy = document.getElementById("btnHoy");
+	const bntMes = document.getElementById("btnMes");
+	const btnAno = document.getElementById("btnAno");
+	const btnAgregarEgreso = document.getElementById("btnAgregarEgreso");
+	const btnAgregarIngreso = document.getElementById("btnAgregarIngreso");
+
+
+	function datosIngresoEgreso(filtroFecha) {
+		consulta = filtroFecha;
 		$.ajax({
 			url: "/listaIngresoEgreso",
-			method:"POST",
-			data:{consulta:consulta},
-			success:function (data) {
+			method: "POST",
+			data: {
+				consulta: consulta
+			},
+			success: function(data) {
 				$('#ingresosEgresos').html(data)
 				$("#ingresosEgresos").append(data.htmlresponse);
-			}	
+			}
 		});
 	}
 
-	datosIngresoEgreso();
+	function nuevoIngresoEgreso(monto,descripcion,fecha, url) {
+		$.ajax({
+			url: url,
+			method: "POST",
+			data: {
+				monto:monto,
+				descripcion:descripcion,
+				fecha:fecha
+			},
+			success: (data) => {
+				$.notify('Agregado correctamente', 'success');
+				datosIngresoEgreso("todos");
+			},
+			error: (error) => {
+				$.notify('Ha ocurrido un grave error, vuelve a intentarlo mas tarde', 'danger');
+			}
+		});
+	}
+
+	btnAgregarEgreso.onclick = () => {
+		let fecha = $('#fechaModalIE').val();
+		let descripcion = $('#descripcionIE').val();
+		let monto = $('#montoIE').val();
+		url = "/nuevoegreso";
+
+		if (descripcion != " " && monto != " " && fecha != " ") {
+			nuevoIngresoEgreso(monto,descripcion,fecha, url);
+			$('#fechaModalIE').val(" ");
+			$('#descripcionIE').val(" ");
+			$('#montoIE').val(" ");
+			$('#modalEgresoIngreso').modal('hide');
+		}else{
+			$.notify('Los campos no pueden estar vacios', 'warning');
+		}
+	}
+
+	btnAgregarIngreso.onclick = () => {
+		let fecha = $('#fechaModalIE').val();
+		let descripcion = $('#descripcionIE').val();
+		let monto = $('#montoIE').val();
+		url = "/nuevoingreso";
+
+		if (descripcion != " " && monto != " " && fecha != "") {
+			nuevoIngresoEgreso(monto,descripcion,fecha, url);
+			$('#fechaModalIE').val("");
+			$('#descripcionIE').val(" ");
+			$('#montoIE').val(" ");
+			$('#modalEgresoIngreso').modal('hide');
+		}else{
+			$.notify('Los campos no pueden estar vacios', 'warning');
+		}
+	}
+
+	btnHoy.onclick = () => {
+		datosIngresoEgreso("hoy");
+	}
+
+	btnMes.onclick = () => {
+		datosIngresoEgreso("mes");
+	}
+
+	btnAno.onclick = () => {
+		datosIngresoEgreso("ano");
+	}
+	datosIngresoEgreso("todos");
+
+
 })
