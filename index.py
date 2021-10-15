62aa4f41dae75c56e87d8788af9d5dd2072f2ac1
Arnaldo Borda <aborda001@gmail.com>
2021-10-14 22:47:54 -0300

    Separando las funciones

--- index.py
+++ index.py
@@ -1,48 +1,9 @@
 from flask import Flask, request, jsonify, render_template, url_for
 import sqlite3
-from datetime import date
+from funciones import *
 
 baseDeDatos = 'database.db'
 app = Flask(__name__)
-
-def ultimoId():
-	conexion = sqlite3.connect(baseDeDatos)
-	cursor = conexion.cursor()
-
-	cursor.execute("SELECT idVenta FROM Venta ORDER BY idVenta DESC LIMIT 1")
-
-	idVenta = cursor.fetchone()
-	cursor.close()
-	conexion.close()
-
-	return idVenta[0]
-
-def filtroFecha(array,filtro):
-	anoActual,mesActual,diaActual = str(date.today()).split("-")
-	nuevoArray = []
-	total = 0
-	if filtro == "hoy":
-		for elemento in array:
-			diaArray,mesArray,anoArray = elemento[3].split("/")
-			if diaArray == diaActual:
-				nuevoArray.append(elemento)
-				total += int(elemento[1])
-		return nuevoArray,total
-	elif filtro == "mes":
-		for elemento in array:
-			diaArray,mesArray,anoArray = elemento[3].split("/")
-			if mesArray == mesActual:
-				nuevoArray.append(elemento)
-				total += int(elemento[1])
-		return nuevoArray,total
-	elif filtro == "ano":
-		for elemento in array:
-			diaArray,mesArray,anoArray = elemento[3].split("/")
-			if anoArray == anoActual:
-				nuevoArray.append(elemento)
-				total += int(elemento[1])
-		return nuevoArray,total
-
 
 @app.route("/")
 def inicio():
